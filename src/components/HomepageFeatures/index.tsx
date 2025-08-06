ad9675cd633ae45a47de950f6148640826b22280
Ryota-Okumuraa <okumurahaku3298@icloud.com>
2025-08-06 13:06:51 +0900

    index-pageの変更

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,32 +11,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'アプリ概要',
+    Svg: require('@site/static/img/appoverview.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        アプリ概要が入りますアプリ概要が入ります
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'デザイン定義',
+    Svg: require('@site/static/img/design.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        デザイン定義が入りますデザイン定義が入ります
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'チームメンバー',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        チームメンバーが入ります。チームメンバーが入ります
       </>
     ),
   },
