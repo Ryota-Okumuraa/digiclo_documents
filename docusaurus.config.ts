2145ca032312476207853dab9792ece731199005
Ryota-Okumuraa <okumurahaku3298@icloud.com>
2025-08-07 11:51:54 +0900

    complete

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
       {
         docs: {
           path: 'docs',
-          sidebarPath: 'false',
+          sidebarPath: false,
           routeBasePath: '/',
           editUrl:
             'https://github.com/Ryota-Okumuraa/digiclo_documents/tree/main/',
